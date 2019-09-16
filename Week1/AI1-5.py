3fe182acf7c8dbf51f403e68a8d0c086844711a5
Jelmer <jelmer.internet@xs4all.nl>
2019-09-17 00:23:25 +0200

    added some more test cases and doc

--- Week1/AI1-5.py
+++ Week1/AI1-5.py
@@ -1,20 +1,28 @@
 import copy
 from collections import deque
-#Setup starting position
-# 8 6 7 
-# 2 5 4 
-# 3 0 1
+#Setup starting position (Randomly choosen)
+# 0 5 2
+# 1 4 3 
+# 7 8 6
+
 # This position is 10 moves away from the desired position
 #Desired position
 # 1 2 3
 # 4 5 6
 # 7 8 0
-board = [['0','5','2'],['1','4','3'],['7','8','6']]
+
+#Some more test cases
+#board = [['0','5','2'],['1','4','3'],['7','8','6']]
+#board = [['2','4','0'],['1','6','3'],['7','5','8']]
+board = [['2','4','3'],['1','5','6'],['0','7','8']]
+#board = [['2','0','3'],['1','4','5'],['7','8','6']]
+
 des_board = [['1','2','3'],['4','5','6'],['7','8','0']]
 x = len(board[0])
 y = len(board)
 heurstic = False
 
+#Small function that prints the board in an easy readable format
 def print_board(board_to_print):
 	#Print board
 	for row in board_to_print:
@@ -26,27 +34,31 @@ def print_board(board_to_print):
 
 #This program is tested with the board above, but it should work on any N*N board
 def program():
-	print("Solved in ", len(bfs(board))," moves")
+	res = bfs(board)
+	if res[0]:
+		print("Solved in ", len(res[1])," moves")
+	else:
+		print("Could not solve this board")
 
+#Basic BFS algorithm, adds children of a node (board state) to the left (start) of a queue and pops off a new node from the right (end) of the queue, bassicly it works like a pipeline
 def bfs(start_node):
-	visited = list()
+	visited = []
 	queue = deque([start_node])
 
 	while len(queue) > 0:
+		#print(queue)
 		node = queue.pop()
 		if parse_board(node) in visited:
 			continue
 		print_board(node)
 		visited.append(parse_board(node))
 		if parse_board(node) == parse_board(des_board):
-			return visited
-
-		for child in find_new_states(node):
+			return [True,visited]
+		for child in find_new_states(node, visited):
 			if parse_board(child) not in visited:
 				queue.appendleft(child)
-	return False
-
-#Finds the possible new states of the board
+	return [False]
+#Finds the possible new states of the board, if heuristic is turned on (True) it will also choose the best new state conform the find_best_state() function
 def find_new_states(board_state_current,visited=[]):
 	zero_pos = []
 	#Find the position of 0
@@ -57,7 +69,7 @@ def find_new_states(board_state_current,visited=[]):
 				break
 
 	up,down,left,right = -1,-1,-1,-1
-	#Look 4 ways, if a way goes beyond board limits it stays a -1
+	#Look 4 directions, if a direction goes beyond board limits it stays a -1
 	if val_num(zero_pos[0]-1,x): #Because the board is always a square it doesn't matter wether x or y is the limit
 		up = [zero_pos[0]-1,zero_pos[1]]
 
@@ -70,26 +82,25 @@ def find_new_states(board_state_current,visited=[]):
 	if val_num(zero_pos[1]+1,x):
 		right = [zero_pos[0],zero_pos[1]+1]
 
-	#For every way get the new board state if the number and 0 would be switched
-	#If a way is invalid (-1) the state would be False
+	#For every direction; get the new board state if the number and 0 would be switched (sliding over the number to the empty spot)
+	#If a direction is invalid (-1) the state would be False
 	state1 = switch_num(zero_pos,up,board_state_current)
 	state2 = switch_num(zero_pos,down,board_state_current)
 	state3 = switch_num(zero_pos,right,board_state_current)
 	state4 = switch_num(zero_pos,left,board_state_current)
 
-
-	#If a state is not False, add it to new_states
+	#If a state is not False (valid) and it hasn't been visited yet, add it to new_states
 	new_states = []
-	if state1 and parse_board(state1) not in visited: 
+	if state1 and parse_board(state1) not in visited:
 		new_states.append(state1)
-	if state2 and parse_board(state2) not in visited: 
+	if state2 and parse_board(state2) not in visited:
 		new_states.append(state2)
-	if state3 and parse_board(state3) not in visited: 
+	if state3 and parse_board(state3) not in visited:
 		new_states.append(state3)
-	if state4 and parse_board(state4) not in visited: 
+	if state4 and parse_board(state4) not in visited:
 		new_states.append(state4)
 	if heurstic: 
-		successors = find_best_state(new_states)
+		successors = find_best_state(new_states) #If heuristic is True, give the list of children to the find_best_state function that will return a list of best options
 	else: 
 		successors = new_states.copy()
 	return successors
@@ -112,7 +123,7 @@ def val_num(num, lim):
 		return False
 	return True
 
-#Parses the board to a string to be compared
+#Parses the board to a string to be compared with other board states
 def parse_board(board_state):
 	board_str = ""
 	for row in board_state:
@@ -120,21 +131,20 @@ def parse_board(board_state):
 			board_str += num
 	return board_str
 
-#Checks the current state agains all the visited states to see if a dead-end is reached
-def check_visited(new_state):
-	for state in visited:
-		if state == new_state:
-			return False
-	return True
-
+#Gets the total score of a board, the score is calculated by taking the sum of the number of moves each number has to make to get to his desired spot
 def get_heurstic_value(c_board):
 	current_board_num_dict = {}
 	des_board_num_dict = {}
+
+	#Fill the 2 dicts (current board and desired board) with each number as a key and their position on the board as value
 	for row in range(x):
 		for col in range(y):
 			current_board_num_dict[c_board[row][col]] = [row, col]
 			des_board_num_dict[des_board[row][col]] = [row, col]
+
 	total = 0
+
+	#Calculate the sum of all moves each number needs to reach it desired spot
 	for num in current_board_num_dict:
 		row_pos = current_board_num_dict[num][0]
 		col_pos = current_board_num_dict[num][1]
@@ -146,7 +156,8 @@ def get_heurstic_value(c_board):
 		ver_moves = col_pos - des_col_pos
 		total += (abs(hoz_moves) + abs(ver_moves))
 	return total
-#Uses heurstic values to find the best ways
+
+#Uses the heuristic value from get_heurstic_value to check to best posible option from all its choices
 def find_best_state(board_states):
 	best_h_val = -1
 	options = []
@@ -156,7 +167,7 @@ def find_best_state(board_states):
 			best_h_val = h_val
 			options.append(board_state)
 
-		elif h_val == best_h_val:
+		elif h_val == best_h_val: #If a board state is equaly good as the current option it is also added to the list of options
 			best_h_val = h_val
 			options.append(board_state)
 
@@ -164,6 +175,11 @@ def find_best_state(board_states):
 			best_h_val = h_val
 			options = []
 			options.append(board_state)
+
+	#Add the rest of the options add the back
+	# for board_state in board_states:
+	# 	if board_state not in options:
+	# 		options.append(board_state)
 	return options
 
 program()
