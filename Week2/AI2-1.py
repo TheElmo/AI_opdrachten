f3525c2e0d59d0d89d4a6bf1af712d4f7f26b854
jelmer <jelmer.internet@xs4all.nl>
2019-09-24 12:00:55 +0200

    Some O improvements

--- Week2/AI2-1.py
+++ Week2/AI2-1.py
@@ -22,6 +22,7 @@ def try_all_tours(cities):
     tours = alltours(cities)
     return min(tours, key=tour_length)
 
+#Connects the cities by choosing the nearest city that hasn't been visited yet
 def nearest_neighbor(cities):
     visited_cities = []
     city = next(iter(cities))
@@ -43,52 +44,34 @@ def nearest_neighbor(cities):
     result = visited_cities
     return result
 
+#Uses nearest neighbor but afterwards does 10.000 iterations of 2_opt to try and remove crossings
 def nearest_neighbor_2opt(cities):
     nn_result = nearest_neighbor(cities)
     #find_linear_functions(cities)
     route = find_crossings(nn_result)
-    for x in range(10000):
-        #print("gone through", x , "times")
-        route = find_crossings(route)
+    old_cost = tour_length(route)
+    # for x in range(10000):
+    #     print("gone through", x , "times")
+    #     route = find_crossings(route)
+    #     new_cost = tour_length(route)
+    #     if old_cost >= new_cost:
+    #         break
     return route
 
-def find_linear_functions(cities):
-    for city in cities:
-        for other_city in cities:
-            if city == other_city:
-                continue
-            line = get_linear(city[0],city[1],other_city[0],other_city[1])
-            key = create_key(city[0],city[1],other_city[0],other_city[1])
-            mirror_key = create_key(other_city[0],other_city[1],city[0],city[1])
-            if mirror_key not in connections:
-                connections[key] = line
 
+#Checks each road with all other roads in the route for crossings, 
+#if a crossing is found it will destroy the 2 roads and create 2 new ones
+#it wil apply this change if the distance of the 2 new roads is shorter then the 2 old ones
 def find_crossings(route):
     visited_combinations = []
-    for i in (range(int(len(route)))):
-        for a in (range(int(len(route)))):
+    for i in (range(len(route))):
+        for a in (range(len(route))):
             if i == a:
                 continue
-            if i+1 == a:
-                continue
-            comb_key = str(i) + str(a)
-            comb_key = "".join(sorted(comb_key))
+            comb_key = (min(i,a),max(i,a))
             if comb_key in visited_combinations:
                 continue
             visited_combinations.append(comb_key)
-            #--------- SWITCH THESE AROUND TO NOTICE TIME DIFF, (Don't forget to switch find_linear_functions(cities) with it)
-            # data = get_connection(route,i)
-            # data2 = get_connection(route,a)
-            # connection1 = data[1]
-            # key1 = data[0]
-            # connection2 = data2[1]
-            # key2 = data2[0]
-
-            # index1 = data[2]
-            # index2 = data[3]
-            # index3 = data2[2]
-            # index4 = data2[3]
-            #---------
             next_i = i+1
             if next_i > len(route)-1:
                 next_i = 0
@@ -99,17 +82,10 @@ def find_crossings(route):
             index2 = next_i
             index3 = a
             index4 = next_a
-
-            key1 = create_key(route[i].x,route[i].y,route[next_i].x,route[next_i].y)
-            key2 = create_key(route[a].x,route[a].y,route[next_a].x,route[next_a].y)
-            #---------
-            segment1 = get_xy_values(key1)
-            segment2 = get_xy_values(key2)
+            segment1 = [route[i].x,route[i].y,route[next_i].x,route[next_i].y]
+            segment2 = [route[a].x,route[a].y,route[next_a].x,route[next_a].y]
             if does_cross(segment1,segment2):
-                # print(segment1, ' and ', segment2, " cross")
-                # print(index1,index2,index3,index4)
                 new_route = route.copy()
-                #NOT SURE HOW TO FLIP, WITHOUT +1 it doesn't work?
                 old_distance = distance(route[min(index1,index2)],route[max(index1,index2)]) + distance(route[min(index3,index4)],route[max(index3,index4)])
                 new_route[index2:index3+1] = reversed(route[index2:index3+1])
                 new_distance = distance(new_route[min(index1,index2)],new_route[max(index1,index2)]) + distance(new_route[min(index3,index4)],new_route[max(index3,index4)])
@@ -117,37 +93,7 @@ def find_crossings(route):
                     route = new_route.copy()
     return route
 
-def get_connection(route,index):
-    next_index = index+1
-    if next_index > len(route)-1:
-        next_index = 0
-    next_city = route[next_index]
-    city = route[index]
-    # print(city,next_city)
-    x1,y1 = city[0], city[1]
-    x2,y2 = next_city[0], next_city[1]
-    connection = None
-    try:
-        key = create_key(x1,y1,x2,y2)
-        connection = connections[key]
-    except KeyError:
-        try:
-            key = create_key(x2,y2,x1,y1)
-            connection = connections[key]
-        except KeyError:
-            print("Help")
-            pass
-    return [key,connection,index,next_index]
-
-def get_xy_values(key):
-    city1 = key.split("|")[0]
-    city2 = key.split("|")[1]
-    x_value1 = int(city1.split(":")[0])
-    y_value1 = int(city1.split(":")[1])
-    x_value2 = int(city2.split(":")[0])
-    y_value2 = int(city2.split(":")[1])
-    return [x_value1,y_value1,x_value2,y_value2]
-
+#Returns True if 2 line segments cross
 def does_cross(segment1,segment2):
     #Naar https://stackoverflow.com/questions/3838329/how-can-i-check-if-two-segments-intersect
     x1 = segment1[0]
@@ -184,40 +130,6 @@ def does_cross(segment1,segment2):
     else:
         return True
 
-@lru_cache(maxsize=None)
-def linear_function(a,x,b):
-    return (a*x) +b
-
-@lru_cache(maxsize=None)
-def get_linear(x1,y1,x2,y2):
-    points = [(x1,y1),(x2,y2)]
-    x_coords, y_coords = zip(*points)
-    val = vstack([x_coords,ones(len(x_coords))]).T
-    a, b = lstsq(val, y_coords,rcond=None)[0]
-    a = float(a)
-    b = float(b)
-    return [a,b]
-
-def create_key(x1,y1,x2,y2):
-    return "{0:d}:{1:d}|{2:d}:{3:d}".format(x1,y1,x2,y2)
-
-def cost(route):
-    finished = False
-    iterator = iter(route)
-    city = next(iterator)
-    previous_city = deepcopy(city)
-    total_distance =0
-    while not finished:
-        try:
-            city = next(iterator)
-        except StopIteration:
-            finished = True
-        else:
-            con_distance = distance(previous_city,city)
-            total_distance += con_distance
-            previous_city = deepcopy(city)
-    return total_distance
-
 def alltours(cities):
     # return a list of tours (a list of lists), each tour a permutation of cities,
     # and each one starting with the same city
@@ -234,7 +146,7 @@ def tour_length(tour):
 def make_cities(n, width=1000, height=1000):
     # make a set of n cities, each with random coordinates within a rectangle (width x height).
 
-    random.seed(1293862394623946) # the current system time is used as a seed
+    random.seed(1) # the current system time is used as a seed
     # note: if we use the same seed, we get the same set of cities
 
     return frozenset(City(random.randrange(width), random.randrange(height))
@@ -252,8 +164,6 @@ def plot_tsp(algorithm, cities):
     # apply a TSP algorithm to cities, print the time it took, and plot the resulting tour.
     t0 = time.clock()
     tour = algorithm(cities)
-    distance = cost(tour)
-    print("Total distance", distance)
     t1 = time.clock()
     print("{} city tour with length {:.1f} in {:.3f} secs for {}"
           .format(len(tour), tour_length(tour), t1 - t0, algorithm.__name__))
@@ -268,4 +178,4 @@ def plot_tsp(algorithm, cities):
 # Total distance for 500 cities: 797860
 
 #D
-plot_tsp(nearest_neighbor_2opt,make_cities(30))
+plot_tsp(nearest_neighbor_2opt,make_cities(500))
