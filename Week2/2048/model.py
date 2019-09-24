c07ff99c0606c357b44b62f3b264fe33a76b0957
jelmer <jelmer.internet@xs4all.nl>
2019-09-24 15:22:50 +0200

    started working on 20148

---
+++ Week2/2048/model.py
@@ -0,0 +1,238 @@
+import random
+import itertools
+import math
+import copy
+
+MAX_DEPTH = 3
+def merge_left(b):
+    # merge the board left
+    # this is the funcyoin that is reused in the other merges
+    # b = [[0, 2, 4, 4], [0, 2, 4, 8], [0, 0, 0, 4], [2, 2, 2, 2]]    
+    def merge(row, acc):
+        # recursive helper for merge_left
+
+        # if len row == 0, return accumulator
+        if not row:
+            return acc
+
+        # x = first element
+        x = row[0]
+        # if len(row) == 1, add element to accumulator
+        if len(row) == 1:
+            return acc + [x]
+
+        # if len(row) >= 2
+        if x == row[1]:
+            # add row[0] + row[1] to accumulator, continue with row[2:]
+            return merge(row[2:], acc + [2 * x])
+        else:
+            # add row[0] to accumulator, continue with row[1:]
+            return merge(row[1:], acc + [x])
+
+    new_b = []
+    for row in b:
+        # merge row, skip the [0]'s
+        merged = merge([x for x in row if x != 0], [])
+        # add [0]'s to the right if necessary
+        merged = merged + [0] * (len(row) - len(merged))
+        new_b.append(merged)
+    # return [[2, 8, 0, 0], [2, 4, 8, 0], [4, 0, 0, 0], [4, 4, 0, 0]]
+    return new_b
+
+def merge_right(b):
+    # merge the board right
+    # b = [[0, 2, 4, 4], [0, 2, 4, 8], [0, 0, 0, 4], [2, 2, 2, 2]]
+    def reverse(x):
+        return list(reversed(x))
+
+    # rev = [[4, 4, 2, 0], [8, 4, 2, 0], [4, 0, 0, 0], [2, 2, 2, 2]]
+    rev = [reverse(x) for x in b]
+    # ml = [[8, 2, 0, 0], [8, 4, 2, 0], [4, 0, 0, 0], [4, 4, 0, 0]]
+    ml = merge_left(rev)
+    # return [[0, 0, 2, 8], [0, 2, 4, 8], [0, 0, 0, 4], [0, 0, 4, 4]]
+    return [reverse(x) for x in ml]
+
+
+def merge_up(b):
+    # merge the board upward
+    # note that zip(*b) is the transpose of b
+    # b = [[0, 2, 4, 4], [0, 2, 4, 8], [0, 0, 0, 4], [2, 2, 2, 2]]
+    # trans = [[2, 0, 0, 0], [4, 2, 0, 0], [8, 2, 0, 0], [4, 8, 4, 2]]
+    trans = merge_left(zip(*b))
+    # return [[2, 4, 8, 4], [0, 2, 2, 8], [0, 0, 0, 4], [0, 0, 0, 2]]
+    return [list(x) for x in zip(*trans)]
+
+
+def merge_down(b):
+    # merge the board downward
+    # b = [[0, 2, 4, 4], [0, 2, 4, 8], [0, 0, 0, 4], [2, 2, 2, 2]]
+    # trans = [[0, 0, 0, 2], [0, 0, 2, 4], [0, 0, 8, 2], [4, 8, 4, 2]]
+    trans = merge_right(zip(*b))
+    # return [[0, 0, 0, 4], [0, 0, 0, 8], [0, 2, 8, 4], [2, 4, 2, 2]]
+    return [list(x) for x in zip(*trans)]
+
+
+# location: after functions
+MERGE_FUNCTIONS = {
+    'left': merge_left,
+    'right': merge_right,
+    'up': merge_up,
+    'down': merge_down
+}
+
+def move_exists(b):
+    # check whether or not a move exists on the board
+    # b = [[1, 2, 3, 4], [5, 6, 7, 8]]
+    # move_exists(b) return False
+    def inner(b):
+        for row in b:
+            for x, y in zip(row[:-1], row[1:]):
+                # tuples (1, 2),(2, 3),(3, 4),(5, 6),(6, 7),(7, 8)
+                if x == y or x == 0 or y == 0:
+                    return True
+        return False
+
+    if inner(b) or inner(zip(*b)):
+        return True
+    else:
+        return False
+
+def start():
+    # make initial board
+    b = [[0] * 4 for _ in range(4)]
+    add_two_four(b)
+    add_two_four(b)
+    return b
+
+
+def play_move(b, direction):
+    # get merge functin an apply it to board
+    b = MERGE_FUNCTIONS[direction](b)
+    add_two_four(b)
+    return b
+
+
+def add_two_four(b):
+    # add a random tile to the board at open position.
+    # chance of placing a 2 is 90%; chance of 4 is 10%
+    rows, cols = list(range(4)), list(range(4))
+    random.shuffle(rows)
+    random.shuffle(cols)
+    distribution = [2] * 9 + [4]
+    for i, j in itertools.product(rows, rows):
+        if b[i][j] == 0:
+            b[i][j] = random.sample(distribution, 1)[0]
+            return (b)
+        else:
+            continue
+
+def game_state(b):
+    for i in range(4):
+        for j in range(4):
+            if b[i][j] >= 2048:
+                return 'win'
+    return 'lose'
+
+def test():
+    b = [[0, 2, 4, 4], [0, 2, 4, 8], [0, 0, 0, 4], [2, 2, 2, 2]]
+    assert merge_left(b) == [[2, 8, 0, 0], [2, 4, 8, 0], [4, 0, 0, 0], [4, 4, 0, 0]]
+    assert merge_right(b) == [[0, 0, 2, 8], [0, 2, 4, 8], [0, 0, 0, 4], [0, 0, 4, 4]]
+    assert merge_up(b) == [(2, 4, 8, 4), (0, 2, 2, 8), (0, 0, 0, 4), (0, 0, 0, 2)]
+    assert merge_down(b) == [(0, 0, 0, 4), (0, 0, 0, 8), (0, 2, 8, 4), (2, 4, 2, 2)]
+    assert move_exists(b) == True
+    b = [[2, 8, 4, 0], [16, 0, 0, 0], [2, 0, 2, 0], [2, 0, 0, 0]]
+    assert (merge_left(b)) == [[2, 8, 4, 0], [16, 0, 0, 0], [4, 0, 0, 0], [2, 0, 0, 0]]
+    assert (merge_right(b)) == [[0, 2, 8, 4], [0, 0, 0, 16], [0, 0, 0, 4], [0, 0, 0, 2]]
+    assert (merge_up(b)) == [(2, 8, 4, 0), (16, 0, 2, 0), (4, 0, 0, 0), (0, 0, 0, 0)]
+    assert (merge_down(b)) == [(0, 0, 0, 0), (2, 0, 0, 0), (16, 0, 4, 0), (4, 8, 2, 0)]
+    assert (move_exists(b)) == True
+    b = [[0, 7, 0, 0], [0, 0, 7, 7], [0, 0, 0, 7], [0, 7, 0, 0]]
+    g = Game()
+    for i in range(11):
+        g.add_two_four(b)
+
+def get_random_move():
+    return random.choice(list(MERGE_FUNCTIONS.keys()))
+
+def get_expectimax_move(b):
+    temp_board= copy.deepcopy(b)
+    board_left = play_move(temp_board,"left")
+
+    temp_board= copy.deepcopy(b)
+    board_up = play_move(temp_board,"up")
+
+    temp_board= copy.deepcopy(b)
+    board_right = play_move(temp_board,'right')
+
+    temp_board= copy.deepcopy(b)
+    board_down = play_move(temp_board,'down')
+
+    left_val = get_heuristic_board_value(board_left)
+    right_val = get_heuristic_board_value(board_up)
+    up_val = get_heuristic_board_value(board_right)
+    down_val = get_heuristic_board_value(board_down)
+
+    if left_val == max(left_val,right_val,up_val,down_val):
+        return 'left'
+    elif right_val == max(left_val,right_val,up_val,down_val):
+        return 'right'
+    elif up_val == max(left_val,right_val,up_val,down_val):
+        return 'up'
+    else:
+        return 'down'
+    pass
+
+def check_highest_value_location(location):
+    #Checks if location is top left corner
+    if location[0] == 0 and location[1] == 0:
+        return True
+    return False
+
+def get_heuristic_board_value(b):
+    #Highest val in top left = 100 points
+    #Empty in top left = -100 points
+    #Same values on the same row = 10 points
+    board_score = 0
+    highest_weight = 100
+    highest_value = 0
+    highest_value_location = [0,0]
+    for row in range(4):
+        for col in range(4):
+            val = b[row][col]
+            if val > highest_value:
+                highest_value = val
+                highest_value_location[0] = row
+                highest_value_location[1] = col
+            
+    if check_highest_value_location(highest_value_location):
+        board_score += highest_weight
+    else:
+        board_score -= highest_weight
+    board_score += check_rows_for_heuristic(b)
+    return board_score
+
+def check_cols_for_heuristic(b):
+    valid_cols = []
+    same_line_weight = 10
+
+def check_rows_for_heuristic(b):
+    valid_rows = []
+    same_line_weight = 10
+    for row in b:
+        #print(row)   
+        start_index = 0
+        has_partner = False
+        for index in range(3):
+            index +=1 #Skip the 0 and go until 3
+            if row[index] == 0: #If current tile is empty skip to next
+                continue 
+            if row[start_index] == row[index]: 
+                #If there is atleast one tile in the row with the same value it has a partner
+                has_partner = True
+            if row[start_index] == row[index] or row[index] == 0:
+                #If it has reached the end, is still valid and the row contains uninterupted partners it is a valid row
+                if index == 3 and has_partner:
+                    valid_rows.append(row)
+            else:
+                break
+    return (same_line_weight * len(valid_rows) + -same_line_weight *(4-len(valid_rows)))
